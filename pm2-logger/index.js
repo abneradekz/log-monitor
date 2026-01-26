e788e79f9b00de928e839cffcd25ef01f0596ea5
Abner Serafim Arede <abner.adekz@adekz.com>
2026-01-26 14:32:49 -0300

    PM2-Logger: ajuste de message com tamanho

--- pm2-logger/index.js
+++ pm2-logger/index.js
@@ -58,7 +58,9 @@ function processLog(packet, severity) {
     return;
   }
 
-  let message = `${packet.process.name}-${packet.process.pm_id}: ${packet.data}`.substr(0, 150);
+  const name = (packet.process.name || 'unknown').substr(0, 10).padEnd(10, ' ');
+
+  let message = `${packet.process.pm_id}|${name}| ${packet.data}`.substr(0, 150);
   // explode no espaço para evitar cortar palavras no meio
   const words = message.split(' ');
   if (words.length > 1) {
